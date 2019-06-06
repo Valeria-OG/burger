0f84824bb30be80e114ee8a4f7e0d83b148cdf2b
Valeria <valeria.obreguillermo@yahoo.com>
2019-06-06 18:02:04 -0500

    code almost working

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES (Vegan Burger, yes);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES (Double Meat Burger, yes);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES (King Bacon Burger, yes);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES (Master Burger, yes);
